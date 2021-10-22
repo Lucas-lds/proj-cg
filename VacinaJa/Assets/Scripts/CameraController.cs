c976fc5e40e4f793ac8354d9934cc8af44708695
Lucas-lds <49286712+Lucas-lds@users.noreply.github.com>
2021-10-21 22:54:11 -0300

    Add project

---
+++ VacinaJa/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform _transform;
+    public Transform cameraTransform;
+
+    Vector2 rotacaoMouse;
+    public int sensibilidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotacaoMouse.x = 169;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 controleMouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        rotacaoMouse = new Vector2(rotacaoMouse.x + controleMouse.x * sensibilidade * Time.deltaTime,
+            rotacaoMouse.y + controleMouse.y * sensibilidade * Time.deltaTime);
+
+            _transform.eulerAngles = new Vector3(_transform.eulerAngles.x, rotacaoMouse.x, _transform.eulerAngles.z);
+
+            rotacaoMouse.y = Mathf.Clamp(rotacaoMouse.y, -37, 80);
+
+            cameraTransform.localEulerAngles = new Vector3(-rotacaoMouse.y, cameraTransform.localEulerAngles.y, cameraTransform.localEulerAngles.z);
+    }
+}
---
+++ VacinaJa/Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour
+{
+    void MudarCena (string Game)
+    {     
+        SceneManager.LoadScene(Game);
+    }
+
+    public void JogarJogo ()
+    {
+        MudarCena("Game");
+    }
+
+}
